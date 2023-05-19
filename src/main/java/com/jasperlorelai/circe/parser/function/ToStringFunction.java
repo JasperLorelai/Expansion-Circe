bec84365339531a6820f3fe581d1dcfd0f5e4eeb
JasperLorelai <wolf.haci12@gmail.com>
2023-05-19 14:33:29 +0200

    feat: Add radix parameter
    BREAKING CHANGES: "toString" requires a second parameter - radix. To preserve behaviour, set it to 10, the decimal system.

--- src/main/java/com/jasperlorelai/circe/parser/function/ToStringFunction.java
+++ src/main/java/com/jasperlorelai/circe/parser/function/ToStringFunction.java
@@ -11,22 +11,27 @@ import com.jasperlorelai.circe.parser.expression.NumberLiteralNode;
 public class ToStringFunction extends Function {
 
     private NumberLiteralNode number;
+    private NumberLiteralNode radix;
 
     @NotNull
     @Override
     public List<ParameterType> getParameterTypes() {
-        return NodeType.NUMBER.asParameterTypes();
+        return List.of(
+                NodeType.NUMBER.asParameterType(),
+                NodeType.NUMBER.asParameterType()
+        );
     }
 
     @Override
     public void initializeArguments(List<ExpressionNode> arguments) {
         number = castNumber(arguments.get(0));
+        radix = castNumber(arguments.get(1));
     }
 
     @NotNull
     @Override
     public ExpressionNode execute() {
-        return stringNode(number.value());
+        return stringNode(Integer.toString(number.valueInteger(), radix.valueInteger()));
     }
 
 }
